d11db60d15fa3a9de7298339c2c782276833f876
Mitsu Iwasaki <mi2.postbox@gmail.com>
2017-01-15 22:31:57 +0900

    [add]Chapter 06 test program

---
+++ class_Dropout.py
@@ -0,0 +1,23 @@
+"""This is test program of Deep Learning Training"""
+
+# 学習に関するテクニック
+# Dropout
+# ランダムにニューロンを選択し、そのニューロンを消去することで信号の伝達をストップする
+
+import numpy as np
+
+
+class Dropout:
+    def __init__(self, dropout_ratio=0.5):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+
+    def forward(self, x, train_flg=True):
+        if train_flg:
+            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+            return x * self.mask
+        else:
+            return x * (1.0 - self.dropout_ratio)
+
+    def backward(self, dout):
+        return dout * self.mask
